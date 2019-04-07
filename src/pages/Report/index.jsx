d6633ffe51869bf9ae6609bacaf5ea8fbdd71df6
maullerz <maullerz@gmail.com>
2019-04-07 13:39:41 +0300

    deploy to netlify

--- src/pages/Report/index.jsx
+++ src/pages/Report/index.jsx
@@ -146,8 +146,15 @@ class RelatedReport extends Component {
         </div>
 
         <div className={styles.footer}>
-          <span>All EVE related materials are property of</span>
-          <a href='http://www.ccpgames.com'>CCP Games</a>
+          <div>
+            <span>All EVE related materials are property of</span>
+            <a href='https://www.ccpgames.com' target='_blank' rel='noopener noreferrer'>
+              CCP Games
+            </a>
+          </div>
+          <a href='https://github.com/maullerz/breport' target='_blank' rel='noopener noreferrer'>
+            GitHub
+          </a>
         </div>
       </div>
     )
