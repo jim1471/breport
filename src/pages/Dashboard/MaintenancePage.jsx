d0e0c9d761744d9cb599d2d837c17edeceefeb15
maullerz <maullerz@gmail.com>
2020-06-09 22:50:43 +0300

    Work in progress

--- src/pages/Dashboard/MaintenancePage.jsx
+++ src/pages/Dashboard/MaintenancePage.jsx
@@ -11,8 +11,13 @@ export default class MaintenancePage extends Component {
           <h4>This tool is for generating battle reports from killmails held on zKillboard.com.</h4>
 
           <div className={styles.maintenanceRoot}>
-            <div>Maintenance</div>
-            <div>...</div>
+            <div>Under Construction</div>
+            <div>.</div>
+            <div>API for relateds used by this website was abused.</div>
+            <div>So it was removed from zKillboard.</div>
+            <div>Some new functionality needs to be done. Work in progress.</div>
+            <div>.</div>
+            <div>Stay tuned and fly safe o7</div>
             {false &&
               <a href='https://status.heroku.com/'>
                 https://status.heroku.com/
